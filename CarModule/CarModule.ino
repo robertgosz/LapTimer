1eb9fe4e0a9489cb35b1a0808d40963c9184988d
root <root@raspberrypi.(none)>
2015-03-08 19:49:35 +0000

    Initial commit

---
+++ CarModule/CarModule.ino
@@ -0,0 +1,74 @@
+
+#include <SPI.h>
+#include "nRF24L01.h"
+#include "RF24.h"
+#include "printf.h"
+
+typedef struct {
+  uint16_t car;
+  uint16_t send_delay;
+  uint16_t retry_count;
+  uint16_t message_no;
+} msgType;
+
+RF24 radio(9,10);
+byte addresses[][6] = {"1Node","2Node"};
+msgType message;
+
+uint16_t car_number = 1;
+uint8_t max_retry_count = 10;
+
+void setup() {
+
+  Serial.begin(115200);
+  printf_begin();
+  SPI.setClockDivider(SPI_CLOCK_DIV4);
+
+  // Setup and configure rf radio
+  radio.begin();                          
+  radio.setAutoAck(1);                    
+  radio.setRetries(15,5);                // Max delay between retries & number of retries
+  radio.setPayloadSize(sizeof(msgType));
+  radio.setChannel(111);
+  radio.setPALevel(RF24_PA_LOW);          // RF24_PA_MIN, RF24_PA_LOW, RF24_PA_HIGH, RF24_PA_MAX
+  radio.openWritingPipe(addresses[0]);
+  radio.openReadingPipe(1,addresses[1]);
+  radio.printDetails();                  
+  
+  message.car = car_number;
+  message.message_no = 1;
+  
+  //printf("Message size: %u \n", sizeof(msgType));
+ 
+}
+
+void loop(void){
+    
+    randomSeed(analogRead(0));
+    unsigned long time_started = micros();
+    unsigned long current_time;
+    boolean success = false;
+    message.retry_count = 1;
+    
+    while (!success && message.retry_count<max_retry_count+1) {
+      current_time = micros();
+      message.send_delay = floor((current_time-time_started)/1000);
+      if (!radio.write(&message, sizeof(msgType))) {
+        printf("Error with %u try \n", message.retry_count);  
+        message.retry_count++;
+        delay(5+random(1, 15));
+      } else {
+        success=true;
+      }      
+    }
+    
+    if (success) {
+      printf("Message sent (msg_no :: try_no :: delay) %u  ::  %u  ::  %u \n", 
+        message.message_no, message.retry_count, message.send_delay);  
+    } else {
+      printf("Transmit failed \n");  
+    }
+
+    delay(1250+random(100, 300));
+    message.message_no++;
+}
---
+++ Receiver/receiver.cpp
@@ -0,0 +1,127 @@
+#include <cstdlib>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <map>
+#include "RF24/RF24.h"
+
+using namespace std;
+
+typedef struct {
+	uint16_t car;
+	uint16_t send_delay;
+	uint16_t retry_count;
+	uint16_t message_id;
+} msgType;
+
+typedef struct {
+	uint16_t message_id;
+	long long receive_timestamp;
+} timePairType;
+
+const uint8_t pipes[][6] = {"1Node","2Node"};	// radio send/receive pipes
+const uint16_t id_immortality_time = 4000;		// time threshold for the next event with same id  to be allowed to register again 
+timePairType timePair;											// keeps last message id and  receive time 
+map<uint16_t, timePairType> cars;						// maps car number to the last msg/time pair
+
+/**
+ * Gets current time (ms)
+ */
+long long milis() {
+	struct timeval tp;
+    gettimeofday(&tp, NULL);
+    return (long long) tp.tv_sec * 1000L + tp.tv_usec / 1000;   
+}
+
+/**
+ * Checks if received message should be registered as valid event
+ */
+bool register_event (msgType* message, long long mslong) {
+
+    if(!cars.empty() && cars.count(message->car)) {
+		// car record exists - check if existing car message has same id
+		if (cars[message->car].message_id==message->message_id) {
+				// check id_immortality_time to decide if the old message is valid or the new one
+				if (mslong-cars[message->car].receive_timestamp<id_immortality_time) {
+					// old message still valid - reject current message as a duplicate
+					return false;
+				}  else {
+                    // same id but the new message counts as a new lap
+       				cout << "Lap time: " << milis()-cars[message->car].receive_timestamp;
+                    printf("\n");
+                }
+			} else {
+				cout << "Lap time: " << milis()-cars[message->car].receive_timestamp;
+				printf("\n");
+				if(message->message_id-cars[message->car].message_id>1) {
+						// Package loss possible (missing id)
+						printf("Package loss possible.  ID - current :  %u last:  %u \n", message->message_id,  cars[message->car].message_id);
+				}
+			}
+	}
+	
+	// just add current id/time pair to the map and consider message as registered event 
+	timePair.message_id=message->message_id;
+	timePair.receive_timestamp=mslong;
+	cars[message->car]=timePair;
+	return true;
+}
+
+void post_event() {
+	
+}
+
+/**
+ * Initialisation and main loop
+ */
+int main(int argc, char **argv) {
+	
+	msgType message;									// radio message 
+	long long message_time;						// meessage receive time (ms)
+	
+	message.car = 0;
+	message.send_delay = 0;
+	
+	printf("Initializing radio \n");
+	RF24 radio(	
+							RPI_V2_GPIO_P1_22, 
+							RPI_V2_GPIO_P1_24, 
+							BCM2835_SPI_SPEED_4MHZ
+						);
+    radio.begin();
+    radio.setRetries(15,25);
+    radio.setPALevel(RF24_PA_LOW);  
+    radio.setAutoAck(1); 
+    radio.setPayloadSize(sizeof(msgType));
+    radio.setChannel(111);
+    radio.printDetails();
+    radio.openWritingPipe(pipes[1]);
+    radio.openReadingPipe(1,pipes[0]);
+    radio.startListening();
+
+    printf("===== Listening ====\n");
+  	while(1) {
+		
+		if (radio.available()) {
+			radio.read(&message, sizeof(msgType));
+			message_time = milis();
+			if (register_event(&message, message_time)) {
+				printf ("Event registered  (car :: try :: delay :: msg)  %u  ::  %u  ::  %u  ::  %u  \n", 
+					message.car, 
+					message.retry_count, 
+					message.send_delay, 
+					message.message_id
+				);
+				post_event();
+			} else {
+				printf ("Message rejected  \n"); 
+			}
+		} else {
+			// no data available, put the cpu to sleep for some us
+			usleep(1000);
+		}
+		
+	}
+	return 0;
+}
+	
